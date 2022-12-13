f7d1c0ca1f651f5b1f597ab6c9f9a289e09b9bac
Snaflus <casper.riis.lie.nielsen@gmail.com>
2022-12-13 15:19:45 +0000

    Update cat.dart

--- lib/src/domain/cat.dart
+++ lib/src/domain/cat.dart
@@ -1,3 +1,5 @@
+import 'package:intl/intl.dart' as intl;
+
 class Cat {
   final int id;
   final String name;
@@ -8,36 +10,53 @@ class Cat {
   final int date;
   final String? pictureUrl;
 
-  const Cat({
-    required this.id,
-    required this.name,
-    required this.description,
-    required this.place,
-    required this.reward,
-    required this.userId,
-    required this.date,
-    required this.pictureUrl
-  });
+  Cat.json(
+      this.id,
+      this.name,
+      this.description,
+      this.place,
+      this.reward,
+      this.userId,
+      this.date,
+      this.pictureUrl);
+
+  Cat.create(
+      this.name,
+      this.description,
+      this.place,
+      this.reward,
+      this.userId,
+      this.pictureUrl):
+        id = -1,
+        date = (DateTime.now().millisecondsSinceEpoch/1000).floor(); //API stores time in unix seconds
+
+  Cat.createWithoutPicture(
+      this.name,
+      this.description,
+      this.place,
+      this.reward,
+      this.userId):
+        id = -1,
+        date = (DateTime.now().millisecondsSinceEpoch/1000).floor(), //API stores time in unix seconds
+        pictureUrl = null;
 
   // Map toJson() =>{
   //
   // };
 
   factory Cat.fromJson(Map<String, dynamic> json) {
-    return Cat(
-      id: json['id'],
-      name: json['title'],
-      description: json['price'],
-      place: json['place'],
-      reward: json['reward'],
-      userId: json['userId'],
-      date: json['date'],
-      pictureUrl: json['pictureUrl']
+    return Cat.json(json['id'],json['name'],json['price'],json['place'],json['reward'],json['userId'],json['date'],json['pictureUrl']
     );
   }
 
   @override
   String toString() {
-    return "ID:$id, Title:$name, Price:$description";
+    return "ID:$id, Title:$name, Description:$description, Place:$place, Reward:$reward, UserID:$userId, Date:$date";
+  }
+
+  String humanDate() {
+    int unix = date * 1000; //convert back to proper unix from API-unix
+    DateTime time = DateTime.fromMillisecondsSinceEpoch(unix, isUtc: true);
+    return intl.DateFormat.yMMMMEEEEd('da_DK').format(time);
   }
 }
