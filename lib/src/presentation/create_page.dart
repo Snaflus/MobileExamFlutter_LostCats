c3d65f69b2d87de74a58456f693754fb6a3dd1c0
Snaflus <casper.riis.lie.nielsen@gmail.com>
2022-12-14 15:48:52 +0000

    Create cat page

--- lib/src/presentation/create_page.dart
+++ lib/src/presentation/create_page.dart
@@ -3,11 +3,14 @@ import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:mobile_exam_flutter_lostcats/src/presentation/popup_menu.dart';
 
+import '../data/cat_repository.dart';
 import '../data/firebase_providers.dart';
 import '../domain/cat.dart';
 
-final usernameController = TextEditingController();
-final passwordController = TextEditingController();
+final nameController = TextEditingController();
+final descController = TextEditingController();
+final placeController = TextEditingController();
+final rewardController = TextEditingController();
 final _formKey = GlobalKey<FormState>();
 
 class CreatePage extends StatefulWidget {
@@ -19,23 +22,134 @@ class CreatePage extends StatefulWidget {
   State<CreatePage> createState() => _CreatePageState();
 }
 
+class SubmitButton extends ConsumerWidget {
+  const SubmitButton({super.key});
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    var firebase = ref.read(firebaseAuthProvider); //used to pass functions
+    var cats = ref.read(catRepositoryProvider); //used to pass functions
+
+    return Padding(
+      padding: const EdgeInsets.symmetric(vertical: 32),
+      child: SizedBox(
+        width: 200,
+        height: 42,
+        child: ElevatedButton(
+          onPressed: () async {
+            if (_formKey.currentState!.validate()) {
+              try {
+                final operation = await cats.postCat(
+                  Cat.createWithoutPicture(
+                      nameController.text,
+                      descController.text,
+                      placeController.text,
+                      int.parse(rewardController.text),
+                      firebase.currentUser!.email!),
+                );
+              } on Exception catch (e) {
+                debugPrint(e.toString());
+              }
+              Navigator.pop(context);
+            }
+          },
+          child: const Text(
+            'Add cat',
+            style: TextStyle(fontSize: 18),
+          ),
+        ),
+      ),
+    );
+  }
+}
+
 class _CreatePageState extends State<CreatePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
         appBar: AppBar(
           title: Text(widget.title),
-          actions: const [PopupMenu()],
+          //actions: const [PopupMenu()], //logging out on create cat page produces unintended behaviour
         ),
         body: Center(
           child: Padding(
-            padding: const EdgeInsets.only(
-                top: 128 + 64, left: 8, right: 8, bottom: 8),
+            padding:
+                const EdgeInsets.only(top: 8, left: 8, right: 8, bottom: 8),
             child: Form(
               key: _formKey,
               child: Column(
                 mainAxisAlignment: MainAxisAlignment.center,
-                children: [],
+                children: [
+                  Padding(
+                    padding:
+                        const EdgeInsets.symmetric(horizontal: 8, vertical: 16),
+                    child: TextFormField(
+                      validator: (value) {
+                        if (value == null || value.isEmpty) {
+                          return "Input cat's name";
+                        }
+                        return null;
+                      },
+                      controller: nameController,
+                      decoration: const InputDecoration(
+                        border: UnderlineInputBorder(),
+                        labelText: "Cat's name",
+                      ),
+                    ),
+                  ),
+                  Padding(
+                    padding:
+                        const EdgeInsets.symmetric(horizontal: 8, vertical: 16),
+                    child: TextFormField(
+                      validator: (value) {
+                        if (value == null || value.isEmpty) {
+                          return "Input cat's description";
+                        }
+                        return null;
+                      },
+                      controller: descController,
+                      decoration: const InputDecoration(
+                        border: UnderlineInputBorder(),
+                        labelText: "Cat's description",
+                      ),
+                    ),
+                  ),
+                  Padding(
+                    padding:
+                        const EdgeInsets.symmetric(horizontal: 8, vertical: 16),
+                    child: TextFormField(
+                      validator: (value) {
+                        if (value == null || value.isEmpty) {
+                          return "Input cat's place";
+                        }
+                        return null;
+                      },
+                      controller: placeController,
+                      decoration: const InputDecoration(
+                        border: UnderlineInputBorder(),
+                        labelText: "Cat's place",
+                      ),
+                    ),
+                  ),
+                  Padding(
+                    padding:
+                        const EdgeInsets.symmetric(horizontal: 8, vertical: 16),
+                    child: TextFormField(
+                      validator: (value) {
+                        if (value == null || value.isEmpty) {
+                          return "Input cat's reward";
+                        }
+                        return null;
+                      },
+                      controller: rewardController,
+                      decoration: const InputDecoration(
+                        border: UnderlineInputBorder(),
+                        labelText: "Cat's reward",
+                      ),
+                    ),
+                  ),
+                  const SubmitButton(),
+                ],
               ),
             ),
           ),
