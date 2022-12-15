2876bc128a27255d8bc920b524a41cc2527f85e5
Snaflus <casper.riis.lie.nielsen@gmail.com>
2022-12-15 13:58:03 +0000

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -13,7 +13,7 @@ void main() async {
     options: DefaultFirebaseOptions.currentPlatform,
   );
 
-  runApp(ProviderScope(child: MyApp()));
+  runApp(const ProviderScope(child: MyApp()));
 }
 
 class MyApp extends StatelessWidget {
